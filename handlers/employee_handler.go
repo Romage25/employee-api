e112149a2cf4d385ece1900298465e4ee63c6c81
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-06-21 06:29:36 +0800

    feat: complete crud of employees and register the route in main go file

--- handlers/employee_handler.go
+++ handlers/employee_handler.go
@@ -8,6 +8,14 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func GetEmployees(c *gin.Context) {
+	var employees []models.Employee
+
+	database.DB.Find(&employees)
+
+	c.JSON(http.StatusOK, employees)
+}
+
 func CreateEmployee(c *gin.Context) {
 	var employee models.Employee
 
@@ -21,4 +29,64 @@ func CreateEmployee(c *gin.Context) {
 	database.DB.Create(&employee)
 
 	c.JSON(http.StatusCreated, employee)
+}
+
+func UpdateEmployee(c *gin.Context) {
+	var employee models.Employee
+
+	id := c.Param("id")
+
+	if err := database.DB.First(&employee, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Employee not found",
+		})
+		return
+	}
+
+	var input map[string]interface{}
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"error": err.Error(),
+		})
+		return
+	}
+
+	database.DB.Model(&employee).Updates(input)
+
+	c.JSON(http.StatusOK, employee)
+}
+
+func DeleteEmployee(c *gin.Context) {
+	var employee models.Employee
+
+	id := c.Param("id")
+
+	if err := database.DB.First(&employee, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Employee not found",
+		})
+		return
+	}
+
+	database.DB.Delete(&employee)
+
+	c.JSON(http.StatusOK, gin.H{
+		"message": "Employee deleted successfully",
+	})
+}
+
+func GetEmployee(c *gin.Context) {
+	var employee models.Employee
+
+	id := c.Param("id")
+
+	if err := database.DB.First(&employee, id).Error; err != nil {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "Employee not found",
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, employee)
 }
--- main.go
+++ main.go
@@ -12,7 +12,11 @@ func main() {
 
 	r := gin.Default()
 
+	r.GET("/employees", handlers.GetEmployees)
 	r.POST("/employees", handlers.CreateEmployee)
+	r.GET("/employees/:id", handlers.GetEmployee)
+	r.PATCH("/employees/:id", handlers.UpdateEmployee)
+	r.DELETE("/employees/:id", handlers.DeleteEmployee)
 
 	r.Run(":8080")
 }
