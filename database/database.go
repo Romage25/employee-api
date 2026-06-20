ae470f1c623e69cf2671ff690e0e9993e8b58db6
Rodel Gerodias <rodelgerodias25@gmail.com>
2026-06-21 06:13:50 +0800

    feat: add dataabse connection and migratre employee table

---
+++ database/database.go
@@ -0,0 +1,24 @@
+package database
+
+import (
+	"employee-api/models"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+func Connect() {
+	dsn := "root:@tcp(127.0.0.1:3306)/employee_db?charset=utf8mb4&parseTime=True&loc=Local"
+
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&models.Employee{})
+
+	DB = db
+}
